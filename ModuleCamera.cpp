f89b54f9947b9f16fbeef6a94e27468909902a99
Saul Juarez <saulzini@gmail.com>
2020-10-31 17:24:33 -0600

    move forward/backward up/down left/right

--- ModuleCamera.cpp
+++ ModuleCamera.cpp
@@ -1,5 +1,5 @@
 #include "ModuleCamera.h"
-#include "MathGeoLib-master/src/Geometry/Frustum.h"
+//#include "MathGeoLib-master/src/Geometry/Frustum.h"
 #include "MathGeoLib-master/src/Math/float3x3.h"
 #include "SDL.h"
 #include "GL/glew.h"
@@ -7,8 +7,18 @@
 #include "ModuleInput.h"
 ModuleCamera::ModuleCamera()
 {
-	//frustum = new Frustum();
+	//initializing
+	camera_position = float3(0, 1, -2);
+	turn_speed = 0.5f;
+	movement_speed = 0.005f;
 
+	//Setting frustum
+	frustum.SetKind(FrustumSpaceGL, FrustumRightHanded);
+	frustum.SetViewPlaneDistances(0.1f, 200.0f);
+	frustum.SetHorizontalFovAndAspectRatio(DEGTORAD(1) * 90.0f, 1.3f);
+	frustum.SetPos(camera_position);
+	frustum.SetFront(float3::unitZ);
+	frustum.SetUp(float3::unitY);
 }
 
 ModuleCamera::~ModuleCamera()
@@ -27,25 +37,6 @@ update_status ModuleCamera::PreUpdate()
 
 update_status ModuleCamera::Update()
 {
-
-	//Setting frustum
-	Frustum frustum;
-
-	frustum.SetKind(FrustumSpaceGL, FrustumRightHanded);
-	frustum.SetViewPlaneDistances(0.1f, 200.0f);
-	frustum.SetHorizontalFovAndAspectRatio(DEGTORAD(1) * 90.0f, 1.3f);
-	frustum.SetPos(float3(0, 1, -2));
-	frustum.SetFront(float3::unitZ);
-	frustum.SetUp(float3::unitY);
-
-	//Setting View 
-	float3x3 rotationMatrix = float3x3::RotateX(1);// = some rotation value
-	vec oldFront = frustum.Front().Normalized();
-	frustum.SetFront(rotationMatrix.MulDir(oldFront));
-	vec oldUp = frustum.Up().Normalized();
-	frustum.SetUp(rotationMatrix.MulDir(oldUp));
-	LOG("left");
-	
 	//Setting projection
 	float4x4 projectionGL = frustum.ProjectionMatrix().Transposed(); //<-- Important to transpose!
 	
@@ -53,11 +44,17 @@ update_status ModuleCamera::Update()
 	glMatrixMode(GL_PROJECTION);
 	glLoadMatrixf(*(projectionGL.v));
 
+	//Setting View 
+	MoveForward();
+	MoveRight();
+	MoveUp();
+	
+
 	//Passing view matrix to opengl
 	float4x4 view = frustum.ViewMatrix(); //<-- Important to transpose!
 	view.Transpose();
 	glMatrixMode(GL_MODELVIEW);
-	glLoadMatrixf(*view.v);
+	glLoadMatrixf(*(view.v));
 	return UPDATE_CONTINUE;
 }
 
@@ -69,4 +66,67 @@ update_status ModuleCamera::PostUpdate()
 bool ModuleCamera::CleanUp()
 {
 	return true;
+}
+
+void ModuleCamera::MoveForward()
+{
+	if (App->input->GetKey(SDL_SCANCODE_W)) {
+		camera_position = float3(camera_position.x, camera_position.y, camera_position.z + movement_speed);
+		frustum.SetPos(camera_position);
+		LOG("W");
+	}
+	if (App->input->GetKey(SDL_SCANCODE_S)) {
+		camera_position = float3(camera_position.x, camera_position.y, camera_position.z - movement_speed);
+		frustum.SetPos(camera_position);
+		LOG("S");
+	}
+}
+
+void ModuleCamera::MoveRight()
+{
+	if (App->input->GetKey(SDL_SCANCODE_D)) {
+		camera_position = float3(camera_position.x - movement_speed, camera_position.y, camera_position.z );
+		frustum.SetPos(camera_position);
+		LOG("D");
+	}
+	if (App->input->GetKey(SDL_SCANCODE_A)) {
+		camera_position = float3(camera_position.x + movement_speed, camera_position.y, camera_position.z );
+		frustum.SetPos(camera_position);
+		LOG("A");
+	}
+}
+
+void ModuleCamera::MoveUp()
+{
+
+	if (App->input->GetKey(SDL_SCANCODE_Q)) {
+		camera_position = float3(camera_position.x , camera_position.y + movement_speed, camera_position.z);
+		frustum.SetPos(camera_position);
+		LOG("Q");
+	}
+	if (App->input->GetKey(SDL_SCANCODE_E)) {
+		camera_position = float3(camera_position.x, camera_position.y - movement_speed, camera_position.z);
+		frustum.SetPos(camera_position);
+		LOG("E");
+	}
+}
+
+void ModuleCamera::Roll()
+{
+}
+
+void ModuleCamera::Pitch()
+{
+	/*if (App->input->GetKey(SDL_SCANCODE_LEFT)) {
+		float3x3 rotationMatrix = float3x3::RotateX(1);// = some rotation value
+		vec oldFront = frustum.Front().Normalized();
+		frustum.SetFront(rotationMatrix.MulDir(oldFront));
+		vec oldUp = frustum.Up().Normalized();
+		frustum.SetUp(rotationMatrix.MulDir(oldUp));
+		LOG("left");
+	}*/
+}
+
+void ModuleCamera::Yaw()
+{
 }
--- ModuleCamera.h
+++ ModuleCamera.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "Module.h"
 #include "Globals.h"
+#include "MathGeoLib-master/src/Geometry/Frustum.h"
+#include "MathGeoLib-master/src/Math/float3.h"
 
 #define DEGTORAD(angleDegrees) ((angleDegrees) * M_PI / 180.0)
+
+//class Frustum;
+//class float3;
 class ModuleCamera :
 	public Module
 {
@@ -16,7 +21,32 @@ public:
 	update_status Update();
 	update_status PostUpdate();
 	bool CleanUp();
-private:
-	//Frustum frustum;
+
+	void MoveForward();
+	void MoveRight();
+	void MoveUp();
+	void Roll();
+	void Pitch();
+	void Yaw();
+
+	void setMovementSpeed(float value) {
+		if (value <= 0.0f) {
+			return;
+		}
+		movement_speed = value;
+	}
+
+	void setTurnSpeed(float value) {
+		if (value <= 0.0f) {
+			return;
+		}
+		turn_speed = value;
+	}
+
+protected:
+	Frustum frustum;
+	float movement_speed;
+	float turn_speed;
+	float3 camera_position;
 };
 
