09aff9361d618f7155d0b444d5c627e563f1c46c
Saul Juarez <saulzini@gmail.com>
2020-12-19 11:45:33 -0600

    getting data from the model

--- BlackWindow/Source/Core/Components/ComponentFactory.h
+++ BlackWindow/Source/Core/Components/ComponentFactory.h
@@ -2,6 +2,7 @@
 #include "Core/Components/Component.h"
 #include "Core/Components/ComponentTypes.h"
 #include "Core/Components/ComponentMesh.h"
+#include "Core/Components/ComponentTransform.h"
 namespace ComponentFactory
 {
     static Component* CreateComponent(GameObject *owner ,ComponentTypes type){
@@ -9,6 +10,8 @@ namespace ComponentFactory
         {
             case ComponentTypes::MESH:
                 return new ComponentMesh(owner,type);
+            case ComponentTypes::TRANSFORM:
+                return new ComponentTransform(owner, type);
             default:
                 return new Component(owner,type);
         }
