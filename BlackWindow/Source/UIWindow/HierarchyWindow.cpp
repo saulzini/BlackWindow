6f48374f145e4fb99eb7001c68fb3dd3ea191a1a
Saul Juarez <saulzini@gmail.com>
2020-12-24 19:36:05 -0600

    changing traverse

--- BlackWindow/Source/UIWindow/HierarchyWindow.cpp
+++ BlackWindow/Source/UIWindow/HierarchyWindow.cpp
@@ -32,11 +32,11 @@ void HierarchyWindow::Update()
             std::vector<GameObject *> gameObjects = root->GetChildren();
             ImGuiTreeNodeFlags nodeFlags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
 
-            for (int i = 0; i < gameObjects.size(); i++)
+            for (std::vector<GameObject *>::iterator it = gameObjects.begin(); it != gameObjects.end(); ++it)
             {
-                bool nodeOpen = ImGui::TreeNodeEx((void *)(intptr_t)i, nodeFlags, gameObjects[i]->GetName().c_str());
-
-                TreeChildren(nodeFlags, nodeOpen, gameObjects[i] );
+                GameObject *gameObject = (GameObject *)*it;
+                bool open = ImGui::TreeNodeEx((void *)gameObject->GetId(), nodeFlags, gameObject->GetName().c_str());
+                TreeChildren(nodeFlags, open, gameObject);
             }
             ImGui::TreePop();
             ImGui::PopStyleVar();
@@ -46,59 +46,57 @@ void HierarchyWindow::Update()
     }
 }
 
-void HierarchyWindow::TreeChildren(ImGuiTreeNodeFlags nodeFlags, bool isOpen,GameObject *currentNode )
+void HierarchyWindow::TreeChildren(ImGuiTreeNodeFlags nodeFlags, bool isOpen, GameObject *currentNode)
 {
     if (isOpen)
     {
-        std::vector<GameObject*> gameObjects = currentNode->GetChildren();
-        for (std::vector<GameObject*>::iterator it =  gameObjects.begin(); it != gameObjects.end(); ++it) 
-        // for (int i = 0; i < gameObjects.size(); i++)
-        {  
-            GameObject* gameObject =  (GameObject*) *it;
-            if ( ( (GameObject*) *it)->GetChildren().empty())
+        if (currentNode->GetChildren().size() > 0)
+        {
+            nodeFlags |= ImGuiTreeNodeFlags_Leaf;
+        }
+
+        ImGui::PushID(currentNode->GetId());
+
+        if (ImGui::IsItemClicked())
+        {
+            App->editor->consoleWindow->AddLog("Clicked %s", currentNode->GetName().c_str());
+        }
+
+        // Our buttons are both drag sources and drag targets here!
+        if (ImGui::BeginDragDropSource())
+        {
+            // Set payload to carry the index of our item (could be anything)
+            ImGui::SetDragDropPayload("HierarchyNodeZone", &currentNode, sizeof(GameObject *));
+
+            // Display preview (could be anything, e.g. when dragging an image we could decide to display
+            // the filename and a small preview of the image, etc.)
+            ImGui::Text("Swap %s", currentNode->GetName().c_str());
+            ImGui::EndDragDropSource();
+        }
+        if (ImGui::BeginDragDropTarget())
+        {
+            if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("HierarchyNodeZone"))
             {
-                nodeFlags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; // ImGuiTreeNodeFlags_Bullet
-                int id = gameObject->GetId();
-                ImGui::PushID(id);
-                ImGui::TreeNodeEx((void *)(intptr_t) gameObject, nodeFlags, gameObject->GetName().c_str());
+                IM_ASSERT(payload->DataSize == sizeof(GameObject *));
+                GameObject *sourceId = (GameObject *)payload->Data;
 
-                if (ImGui::IsItemClicked()) {
-                    // Some processing...
-                    App->editor->consoleWindow->AddLog("Clicked %s",gameObject->GetName().c_str());
-                }
-
-                // Our buttons are both drag sources and drag targets here!
-                if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
-                {
-                    // Set payload to carry the index of our item (could be anything)
-                    ImGui::SetDragDropPayload("HierarchyNodeZone", &id, sizeof(int));
-
-                    // Display preview (could be anything, e.g. when dragging an image we could decide to display
-                    // the filename and a small preview of the image, etc.)
-                    ImGui::Text("Swap %s", gameObject->GetName().c_str());
-                    ImGui::EndDragDropSource();
-                }
-                if (ImGui::BeginDragDropTarget())
-                {
-                    if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("HierarchyNodeZone"))
-                    {
-                        IM_ASSERT(payload->DataSize == sizeof(int));
-                        int sourceId = *(const int*)payload->Data;
-
-                        App->editor->consoleWindow->AddLog("Dragged %s to %i ", gameObject->GetName().c_str(),sourceId);
-
-                    }
-                    ImGui::EndDragDropTarget();
-                }
-                ImGui::PopID();
+                App->editor->consoleWindow->AddLog("Dragged %s to %i ", currentNode->GetName().c_str(), sourceId);
             }
-            else
+            ImGui::EndDragDropTarget();
+        }
+
+        if (!currentNode->GetChildren().empty())
+        {
+            std::vector<GameObject *> gameObjects = currentNode->GetChildren();
+            for (std::vector<GameObject *>::iterator it = gameObjects.begin(); it != gameObjects.end(); ++it)
             {
-                bool o = ImGui::TreeNodeEx((void *)(intptr_t) gameObject, nodeFlags, gameObject->GetName().c_str());
-                ImGui::TreePop();
-                TreeChildren(nodeFlags, o, gameObject);
+                GameObject *gameObject = (GameObject *)*it;
+                bool open = ImGui::TreeNodeEx((void *)gameObject->GetId(), nodeFlags, gameObject->GetName().c_str());
+                TreeChildren(nodeFlags, open, gameObject);
             }
         }
+        ImGui::PopID();
         ImGui::TreePop();
+
     }
 }
