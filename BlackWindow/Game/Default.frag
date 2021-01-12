e7bf57d8b6e45445d52b82d9e96d4da7e22437a0
telmiuse <61976005+telmiuse@users.noreply.github.com>
2021-01-12 17:30:41 +0100

    New Shaders Version

--- BlackWindow/Game/Default.frag
+++ BlackWindow/Game/Default.frag
@@ -7,27 +7,45 @@ uniform float ks,kd;
 uniform vec3 colorAmbient;
 uniform float N;
 in vec2 uv;
-uniform sampler2D texture_diffuse;
+//uniform sampler2D texture_diffuse;
+//uniform sampler2D texture_Occlusion;
 in vec3 fragment_normal;
 in vec3 FragPos;
 out vec4 color;
+
+
+uniform struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+    
+};
+
+uniform struct Light {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
 
 void main(){
 
 
 	vec3 normal = normalize(fragment_normal);
-	vec3 ambient = colorAmbient * texture(texture_diffuse, uv).rgb;
+	vec3 ambient = light.ambient * vec3(texture( material.diffuse, uv));
 	//-----------------------------------
 
-	vec3 lightDir = normalize(light_pos - FragPos);
-	float nDtoL = max(dot(normal , - lightDir),0.0);
-	vec3 diff = nDtoL * kd * light_color * texture(texture_diffuse, uv).rgb;
+	vec3 lightDir = normalize( light_pos - FragPos);
+	float nDtoL = max(dot(normal ,   lightDir),0.0);
+	vec3 diff = nDtoL  * light.diffuse * vec3(texture(material.diffuse, uv));
 	//------------------------------------
 
-	vec3 reflectDir = reflect(lightDir, normal);
+	vec3 reflectDir = reflect( -lightDir, normal);
 	vec3 V = normalize(viewPos - FragPos);
-	float spec = pow(max(dot(V , reflectDir), 0.0),N);
-	vec3 specular = ks * spec * light_color;
+	float spec = pow(max(dot(V ,  reflectDir), 0.0),material.shininess);
+	vec3 specular =  spec * light.specular *  vec3(texture(material.specular,  uv));
 
 	//-----------------------------------
 
--- BlackWindow/Source/ModuleScene.cpp
+++ BlackWindow/Source/ModuleScene.cpp
@@ -60,6 +60,7 @@ bool ModuleScene::Init()
 	Light = GameObjectFactory::CreateGameObject(GameObjectTypes::LIGHT, root, "Light", program);
 	root->AddChildren(Light);
 	Light->GetTransformComponent()->SetPosition(float3 (0.0f, 1.0f,0.0f));
+	
 	return true;
 }
 
@@ -84,29 +85,35 @@ update_status ModuleScene::Update(float deltaTime)
 
 	GLint ks = glGetUniformLocation(program, "ks");
 	GLint kd = glGetUniformLocation(program, "kd");
-	GLint N = glGetUniformLocation(program, "N");
+	GLint N = glGetUniformLocation(program, "material.shininess");
 
 	GLint light_pos = glGetUniformLocation(program, "light_pos");
-	GLint light_color = glGetUniformLocation(program, "light_color");
+	GLint light_ambient = glGetUniformLocation(program, "light.ambient");
+	GLint light_diffuse = glGetUniformLocation(program, "light.diffuse");
+	GLint light_specular = glGetUniformLocation(program, "light.specular");
 	GLint viewPos = glGetUniformLocation(program, "viewPos");
 	GLint colorAmbient = glGetUniformLocation(program, "colorAmbient");
 
 	glUniform1f(ks, 0.8f);
 	glUniform1f(kd, 0.6f);
-	glUniform1f(N, 32);
+	glUniform1f(N, 255);
 
-	float3 lightpos = { 1.0f, 0.0f, 0.0f };
-	lightpos = Light->GetTransformComponent()->GetPosition();
-	float3 lightcolor = { 1.0f, 1.0f, 1.0f };
+	//float3 lightpos = { 1.0f, 0.0f, 0.0f };
+	float3 lightpos = Light->GetTransformComponent()->GetPosition();
+	float3 lightambient = { 0.2f, 0.2f, 0.2f };
+	float3 lightdiffuse = { 0.5f, 0.5f, 0.5f };
+	float3 lightspecular = { 1.0f, 1.0f, 1.0f };
 	float3 view_Pos = App->camera->cameraPosition;
 	float3 color_Ambient = { 1.0f, 1.0f, 1.0f };
 
-	glUniform1i(glGetUniformLocation(program, "texture_diffuse"), 0);
+	glUniform1i(glGetUniformLocation(program, "material.diffuse"), 0);
 
-	glUniform3f(light_pos,		   lightpos[0],		 lightpos[1],		lightpos[2]);
-	glUniform3f(light_color,	 lightcolor[0],    lightcolor[1],	  lightcolor[2]);
-	glUniform3f(viewPos,		   view_Pos[0],		 view_Pos[1],		view_Pos[2]);
-	glUniform3f(colorAmbient, color_Ambient[0], color_Ambient[1],  color_Ambient[2]);
+	glUniform3f(light_pos,			 lightpos[0],			 lightpos[1],				lightpos[2]);
+	glUniform3f(light_ambient,   lightambient[0],		 lightambient[1],			lightambient[2]);
+	glUniform3f(light_diffuse,	 lightdiffuse[0],		 lightdiffuse[1],			lightdiffuse[2]);
+	glUniform3f(light_specular, lightspecular[0],		lightspecular[1],		   lightspecular[2]);
+	glUniform3f(viewPos,			 view_Pos[0],			 view_Pos[1],				view_Pos[2]);
+	glUniform3f(colorAmbient,	color_Ambient[0],		color_Ambient[1],		   color_Ambient[2]);
 
 	std::cout << glGetError() << std::endl; // returns 0 (no error)
 	
